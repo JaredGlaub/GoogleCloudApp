6d79a1552206392955910589625f6ddb087dd94d
JaredGlaub <jglaub1311@gmail.com>
2021-10-21 21:05:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,22 +20,24 @@ def helloWorld():
 
 @app.route('/prediction')
 def predictionResults():
-    bqclient = bigquery.Client()
-    query_string = """SELECT FROM `burnished-ember-328422.census.predictionResults` LIMIT 10"""
+    val = {"value": "hello World"}
+    return jsonify(val)
+#     bqclient = bigquery.Client()
+#     query_string = """SELECT FROM `burnished-ember-328422.census.predictionResults` LIMIT 10"""
 
-    df = (
-        bqclient.query(query_string)
-        .result()
-        .to_dataframe(
-            # Optionally, explicitly request to use the BigQuery Storage API. As of
-            # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
-            # API is used by default.
-            create_bqstorage_client=True,
-        )
-    )
-    shorten = df.head(10)
-    json_results = shorten.to_json()
-    return json_results
+#     df = (
+#         bqclient.query(query_string)
+#         .result()
+#         .to_dataframe(
+#             # Optionally, explicitly request to use the BigQuery Storage API. As of
+#             # google-cloud-bigquery version 1.26.0 and above, the BigQuery Storage
+#             # API is used by default.
+#             create_bqstorage_client=True,
+#         )
+#     )
+#     shorten = df.head(10)
+#     json_results = shorten.to_json()
+#     return json_results
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
