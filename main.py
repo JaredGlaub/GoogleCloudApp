e453407ca1977a834578558fcb70b72e0ceeee3e
JaredGlaub <jglaub1311@gmail.com>
2021-11-26 23:58:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -45,4 +45,4 @@ def predictionResults():
     return json_results
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='https://burnished-ember-328422.nw.r.appspot.com/', debug=True)
