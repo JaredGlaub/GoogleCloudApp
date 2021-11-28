740293ca044ae48a65c7233e70f6f3e9087f5d3d
Jared Glaub <jglaub1311@gmail.com>
2021-11-28 05:14:55 +0000

    rendering with parenthesis

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes, HashRouter} from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import Animated from "./pages/Animated";
@@ -8,13 +8,13 @@ import DoodleNet from "./pages/DoodleNet";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<Landing/>} />
           <Route path='/Animated' element={<Animated/>} />
           <Route path='/Training' element={<Training/>} />
           <Route path='/DoodleNet' element={<DoodleNet/>} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
